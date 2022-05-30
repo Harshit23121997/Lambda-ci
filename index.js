b2f3b195d06b7a98e53fa03703c2c4d66b31e28c
Harshit <harshitgupta226487@gmail.com>
2022-05-31 01:12:36 +0530

    cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const http = require('http')
 
-var app = express();    
+var app = express();   
+var cors = require('cors') 
 
 //AKIAZQSXQGB4HXAJLQDN
 //O8uaXb80lxkoek57v/hcRzX9c1m2nUTjzREwWta
 
+
 var httpServer = http.createServer(app);
+app.use(cors())
 
 app.get('/', function (req,res){
     console.log("Here")
